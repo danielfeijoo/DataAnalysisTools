957549a773d8191934dfeabbd535d458a1f9ce42
danielfeijoo <156625054+danielfeijoo@users.noreply.github.com>
2024-04-27 20:13:10 -0400

    Delete ScatterPlot.R

--- ScatterPlot.R
+++
@@ -1,8 +0,0 @@
-# Function to create a scatter plot
-#' Create a scatter plot to visualize the relationship between two numeric variables.
-#' @param data The dataset containing the numeric variables to be visualized.
-#' @param x The name of the first numeric variable.
-#' @param y The name of the second numeric variable.
-#' @return A scatter plot showing the relationship between the specified numeric variables.
-create_scatter_plot <- function(data, x, y) {
-  # Function implementation
