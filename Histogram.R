7c4c4891af21e6f867d71a4839af04032c95f1c4
danielfeijoo <156625054+danielfeijoo@users.noreply.github.com>
2024-04-27 20:03:06 -0400

    Create Histogram.R

---
+++ Histogram.R
@@ -0,0 +1,7 @@
+# Function to create a histogram
+#' Generate a histogram to visualize the distribution of a numeric variable.
+#' @param data The dataset containing the numeric variable to be visualized.
+#' @param x The name of the numeric variable to be plotted.
+#' @return A histogram plot displaying the distribution of the specified numeric variable.
+create_histogram <- function(data, x) {
+  # Function implementation
