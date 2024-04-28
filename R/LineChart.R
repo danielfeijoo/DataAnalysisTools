c2d3f901b836f153847af433746a663054932416
danielfeijoo <156625054+danielfeijoo@users.noreply.github.com>
2024-04-27 20:13:56 -0400

    Create LineChart.R

---
+++ R/LineChart.R
@@ -0,0 +1,8 @@
+# Function to create a line chart
+#' Produce a line chart to visualize the trend of a numeric variable over time or another continuous variable.
+#' @param data The dataset containing the variables to be plotted.
+#' @param x The name of the continuous variable for the x-axis.
+#' @param y The name of the numeric variable for the y-axis.
+#' @return A line chart illustrating the trend of the specified numeric variable over the continuous variable.
+create_line_chart <- function(data, x, y) {
+  # Function implementation
